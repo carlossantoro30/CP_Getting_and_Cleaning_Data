4a859fa1971f623636362807f73c71685ef1fc3e
Carlos Santoro <carlos.santoro@hotmail.com>
2018-12-25 22:31:57 -0200

    SRefresh 3

--- run_analysis.R
+++ run_analysis.R
@@ -7,19 +7,37 @@
 
 print(paste("Working directory: ",getwd(), sep = "", collapse = NULL))
 
-#Source the required functions
-script_path <- "./Functions.R"
-source(script_path)
+#Source the required packages and functions
+source("./Functions.R")
 
 #Get data
 download.data()
 
+#Read all data required
 features <- read_column("./UCI HAR Dataset/features.txt")
-features <- features[-seq(from = 1, to = length(features), by = 2)]
-
+features <- features[-seq(from = 1, to = length(features), by = 2)]     #Clean the feature vector
 train_data <- read_dataset("train", features)
 test_data <- read_dataset("test", features)
+labels <- read_sets("./UCI HAR Dataset/activity_labels.txt")
 
+#Merge datasets
+names(train_data)[4] <- "Value.Train"
+names(test_data)[4] <- "Value.Test"
+dataset <- full_join(x = train_data, y = test_data, by = c("y", "subject", "Feature"))
+names(labels) <- c("y", "Activity.Label")
+dataset <- left_join(x = dataset, y = labels, by = "y")       #Put descreptive activity names
+
+#Manipulation of data
+class(dataset$Value.Train) <- "numeric"
+class(dataset$Value.Test) <- "numeric"
+dataset <- dataset %>%
+        select(-y) %>%
+        filter(grepl("mean|std", Feature))
+final_dataset <- dataset %>%
+        group_by(Activity.Label, subject) %>%
+        summarise(Value.Train = mean(), Value.Test = mean())
+        
+        
 
 
 
